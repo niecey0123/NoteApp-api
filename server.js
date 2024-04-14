f33886e25cc559b6a3bf05450b69853583dbc847
niecey0123 <s.crumpler11@gmail.com>
2024-04-14 14:27:05 -0500

    final deploy

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-// const corsOptions = {
-    // origin: 'https://my-note-app-38wr.onrender.com/',//(https://your-client-app.com)
-    // optionsSuccessStatus: 200,
-//   };
+ const corsOptions = {
+     origin: 'https://my-note-app-38wr.onrender.com/',//(https://your-client-app.com)
+    optionsSuccessStatus: 200,
+ };
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
