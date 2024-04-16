1cc94fe908a426401826ba599484ce7d17fb97b3
niecey0123 <s.crumpler11@gmail.com>
2024-04-16 08:32:15 -0500

    final deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // const path = require('path');
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 // const cors = require('cors');
 const bodyParser = require('body-parser');
 
